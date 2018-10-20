feb1d697f7f8e284471af6449954dcd0ddff788e
Jigar <jigaragrawal29@gmail.com>
2018-10-20 08:38:52 -0700

    File operations added

--- solid_chardev_driver.c
+++ solid_chardev_driver.c
@@ -3,6 +3,7 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/kernel.h>
+#include <linux/semaphore.h> 
 #include <linux/cdev.h> 
 #include <linux/uaccess.h>              // This is used to access the user-space memory from the kernel space, since 
                                         // both the memory sections are different, accessing the user-space is a little bit 
@@ -14,6 +15,12 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jigar Agrawal"); 
 MODULE_DESCRIPTION("Character device driver.");
 MODULE_VERSION("0.0"); 
+
+//Defining the local memory for reading and sriting operations
+char device_buffer[1000] = ""; 
+
+//Declaring the semaphor to be used for the device 
+struct semaphore device_semaphore = {0}; 
 
 // Defines 
 #define DEVICE_NAME "char_device"
@@ -82,7 +89,13 @@ static int __init module_initialization(void)
         unregister_chrdev_region(device_registration, 1); 
         return return_val; 
     }
-    return major_num; 
+    printk(KERN_ALERT"The device has been added to the kernel Successfully."); 
+
+    // Initializing the semaphore for the file operation to avoid data curruption from multiple processes or 
+    // threads writting to this file at the same time 
+    // static inline void sema_init(struct semaphore *sem, int val); 
+    sema_init(&device_semaphore, 1); 
+    return return_val; 
 }
 module_init(module_initialization); 
 
@@ -103,18 +116,42 @@ module_exit(module_deinitialization);
 
 int openTheDevice (struct inode *node, struct file *fptr)
 {
+    // Decrementing the semaphor by 1 to make it 0 and preventing this file to get executed by
+    // multiple threads 
+    // extern int __must_check down_interruptible(struct semaphore *sem);
+    if(down_interruptible(&device_semaphore) != 0)
+    {
+        printk(KERN_ALERT"The device is being operated by some other thread, please let the device become free");
+        return -1; 
+    }
+    printk(KERN_ALERT"The device is opened and ready to be used"); 
     return 0; 
 }
 
 ssize_t readFromDevice (struct file *fptr, char __user *dest_buffer, size_t bytes_to_read, loff_t *cur_pos)
 {
-    return 0; 
+    // Reading the given number of bytes from the device 
+    // Static __always_inline unsigned long copy_to_user(void __user *to, const void *from, unsigned long n) 
+    printk(KERN_ALERT"Reading from the device"); 
+    return_val = copy_to_user(dest_buffer, &device_buffer[0], bytes_to_read);
+    return return_val; 
 }
 ssize_t writeToDevice (struct file *fptr, const char __user *source_buffer, size_t bytes_to_write, loff_t *cur_pos)
 {
-    return 0; 
+    // Writting the number of bytes to the device 
+    // static inline unsigned long copy_from_user(void *to, const void __user *from, unsigned long n)
+    return_val = copy_from_user(&device_buffer[0], source_buffer, bytes_to_write); 
+    if(return_val >=0)
+    {
+        printk(KERN_ALERT"Wrote the value to the device");
+    }
+
+    return return_val; 
 }
 int closeTheDevice (struct inode *node, struct file *fptr)
 {
+    printk(KERN_ALERT"Closing the device.."); 
+    // Let us release the semaphore for other threads 
+    up(&device_semaphore); 
     return 0; 
 }
